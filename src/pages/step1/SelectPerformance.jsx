55efd0356554789b4ffc13a86c13cd6a9a7bf58a
영 <77565980+abyss-s@users.noreply.github.com>
2024-07-22 04:43:32 +0900

    Feat: 1단계 - 공연 선택, 회차 선택 페이지 구현

---
+++ src/pages/step1/SelectPerformance.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PosterList from "../../components/poster/PosterList";
+import { useAtom } from "jotai";
+import { selectedPosterAtom, levelAtom, progressAtom } from "../../store/atom";
+// import { useNavigate } from "react-router-dom";
+
+const SelectPerformance = () => {
+  const [, setSelectedPoster] = useAtom(selectedPosterAtom); // 선택된 포스터의 id 상태 관리
+  const [, setLevel] = useAtom(levelAtom);
+  const [, setProgress] = useAtom(progressAtom);
+  // const navigate = useNavigate();
+
+  setLevel("high");
+  setProgress(1);
+
+  const handlePosterClick = (posterId) => {
+    setSelectedPoster(posterId);
+    // navigate("/select-round-high");
+  };
+
+  return <PosterList onPosterClick={handlePosterClick} />;
+};
+
+export default SelectPerformance;
