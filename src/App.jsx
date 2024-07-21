0dff048f80c5b88669edc006e6f9dfa0a65b05f1
hanheel <hanhee630630@gmail.com>
2024-07-22 01:57:39 +0900

    Feat: card form 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Layout from "./pages/Layout";
 import ProgressContents from "./pages/ProgressContents";
+import CardForm from "./components/forms/CardForm";
 
 function App() {
   return (
     <Layout>
-      <ProgressContents></ProgressContents>
+      <ProgressContents>
+        <CardForm />
+      </ProgressContents>
     </Layout>
   );
 }
---
+++ src/components/forms/CardForm.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Input from "../input/Input";
+import { useState } from "react";
+
+const FormWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const CardForm = () => {
+  const [isanswer, setIsAnswer] = useState(false);
+  const answer = [];
+  const onChange = (e) => {
+    answer.push(e.target.value);
+    console.log(answer);
+  };
+  return (
+    <FormWrap>
+      <Input text="카드 번호" onChange={onChange} />
+      <Input text="카드 비밀번호" onChange={onChange} />
+      <Input text="cvc 번호" onChange={onChange} />
+    </FormWrap>
+  );
+};
+
+export default CardForm;
