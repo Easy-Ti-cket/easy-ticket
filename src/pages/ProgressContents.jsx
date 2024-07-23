e3ac518e2fbd453ad262a81e1f9db2f179b2b8ac
Ko Daeun <drechsu@example.com>
2024-07-23 14:30:43 +0900

    Refactor: ProgresContens에 TImer, TextBox추가

--- src/pages/ProgressContents.jsx
+++ src/pages/ProgressContents.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import ProgressBar from "../components/ProgressBar";
-
+import Timer from "../components/timer/Timer";
 //ProgressBar+ContentsBox Container
 const ProgressContentsContainer = styled.div`
   display: flex;
@@ -13,6 +13,13 @@ const ProgressBarBox = styled.div`
   min-height: 145px;
 `;
 
+const TextBot = styled.div`
+  width: 1320px;
+  min-height: 122px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 //화면 높이에 따라 줄어드는 contentsBox, min-height: 500px
 const ContentsBox = styled.div`
   width: 1320px;
@@ -30,8 +37,11 @@ const ProgressContents = ({ children }) => {
   return (
     <ProgressContentsContainer>
       <ProgressBarBox>
-        <ProgressBar currentStep={2} />
+        <ProgressBar />
       </ProgressBarBox>
+      <Timer></Timer>
+      {/* FIX: textcomponent 받아서 렌더링 */}
+      <TextBot>예매하려는 공연의 포스터를 선택해주세요</TextBot>
       <ContentsBox>{children}</ContentsBox>
     </ProgressContentsContainer>
   );
