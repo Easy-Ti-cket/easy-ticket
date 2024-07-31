90dd07cb13c72fd7d7cba2ea172ee36c388720e3
다은 <drechsu@gmail.com>
2024-07-31 15:31:44 +0900

    Feat: 컴포넌트 배치

--- src/pages/practiceMode/step2/SelectSeat.jsx
+++ src/pages/practiceMode/step2/SelectSeat.jsx
@@ -1,5 +1,18 @@
+import SeatSection from "../../../components/seatSection/SeatSection";
+import SeatInfo from "../../../components/seatInfo/SeatInfo";
+import styled from "styled-components";
+const SelectSeatcontainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 const SelectSeat = () => {
-  return <></>;
+  return (
+    <SelectSeatcontainer>
+      <SeatSection></SeatSection>
+      <SeatInfo></SeatInfo>
+    </SelectSeatcontainer>
+  );
 };
 
 export default SelectSeat;
