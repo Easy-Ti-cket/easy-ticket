2a9143f36cb0eec5f70b44187465fd22b1e3f208
Ko Daeun <drechsu@gmail.com>
2024-08-13 18:00:04 +0900

    Feat: theme.js에  사이트 이름 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,14 +4,20 @@ import "../index.css";
 const buttonColors = {
   practice: "var(--key-color)",
   //임시 색상
-  interpark: "var(--point-color)"
+  interpark: "var(--point-color)",
+  melonticket: "var(--point-color)",
+  tickelink: "var(--point-color)",
+  yes24: "var(--point-color)"
 };
 
 //프로그레스바 색상 테마 객체
 const progressBarColors = {
   practice: "var(--key-color)",
   //임시 색상
-  interpark: "var(--point-color)"
+  interpark: "var(--point-color)",
+  melonticket: "var(--point-color)",
+  tickelink: "var(--point-color)",
+  yes24: "var(--point-color)"
 };
 
 //내보내는 테마 객체
