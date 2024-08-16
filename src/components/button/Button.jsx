03200d28d35e5494b73ebcae986ec178fae45872
영 <77565980+abyss-s@users.noreply.github.com>
2024-08-17 03:41:54 +0900

    Refactor: 버튼 module.css에 theme별 스타일링 추가 및 atom 적용

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,15 +1,21 @@
+// Button.jsx
 import React from "react";
+import { useAtom } from "jotai";
+import { themeSiteAtom } from "../../store/atom";
 import styles from "./Button.module.css";
 
 const Button = (props) => {
   const { text, onClick, type, icon, ...rest } = props;
+  const [currentTheme] = useAtom(themeSiteAtom);
+
+  const buttonClass = `${styles.button} ${
+    currentTheme ? styles[`button--${currentTheme}`] : ""
+  } ${
+    type === "outline" ? styles[`button--outline--${currentTheme}`] : ""
+  } ${type ? styles[`button--${type}`] : ""}`;
 
   return (
-    <button
-      className={`${styles.button} ${type ? styles[`button--${type}`] : ""}`}
-      onClick={onClick}
-      {...rest}
-    >
+    <button className={buttonClass} onClick={onClick} {...rest}>
       {icon && <img src={icon} alt="" className={styles.icon} />}
       {text}
     </button>
