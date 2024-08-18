d3c76de6fe340c5fd0f62ad9e83eaf2b5335a503
abyss-s <xyz@inha.edu>
2024-08-18 22:23:20 +0900

    Design: theme.js에 var 컬러 변수 적용

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,39 +1,44 @@
 const interpark = {
-  keyColor: "#d40000",
-  hoverColor: "#b50000",
-  subColor: "#00ff50",
-  grayColor: "#2c2c2c"
+  keyColor: "var(--key-color-interpark)",
+  hoverColor: "var(--hover-color-interpark)",
+  subColor: "var(--sub-color-interpark)",
+  grayColor: "var(--gray-color-interpark)"
 };
 
 const melonticket = {
-  keyColor: "#00ff00",
-  hoverColor: "#00b500",
-  subColor: "#00ff50",
-  grayColor: "#2c2c2c"
+  keyColor: "var(--key-color-melonticket)",
+  hoverColor: "var(--hover-color-melonticket)",
+  subColor: "var(--sub-color-melonticket)",
+  grayColor: "var(--gray-color-melonticket)"
 };
 
 const ticketlink = {
-  keyColor: "#00ff00",
-  hoverColor: "#00b500",
-  subColor: "#000050",
-  grayColor: "#2c2c2c"
+  keyColor: "var(--key-color-ticketlink)",
+  hoverColor: "var(--hover-color-ticketlink)",
+  subColor: "var(--sub-color-ticketlink)",
+  grayColor: "var(--gray-color-ticketlink)"
 };
 
 const yes24 = {
-  keyColor: "#00a2ff",
-  hoverColor: "#005bb5",
-  subColor: "#00a2ff50",
-  grayColor: "#2c2c2c"
+  keyColor: "var(--key-color-yes24)",
+  hoverColor: "var(--hover-color-yes24)",
+  subColor: "var(--sub-color-yes24)",
+  grayColor: "var(--gray-color-yes24)"
 };
 
-const practice = {};
+const practice = {
+  keyColor: "var(--key-color)",
+  hoverColor: "var(--hover-color)",
+  subColor: "var(--sub-color)",
+  grayColor: "var(--gray-color)"
+};
 
 const theme = {
   interpark,
   melonticket,
   ticketlink,
   yes24,
-  practice
+  default: practice
 };
 
 export default theme;
