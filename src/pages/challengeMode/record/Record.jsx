215381e7ef2d57fafecb305690e0791dad4d9f6a
hanheel <hanhee630630@gmail.com>
2024-09-03 20:43:13 +0900

    Fix: 렌더링되지 않는 문제 수정

--- src/pages/challengeMode/record/Record.jsx
+++ src/pages/challengeMode/record/Record.jsx
@@ -27,42 +27,40 @@ const Record = () => {
   //세션스토리지에 저장된 record
   const myRecord = JSON.parse(sessionStorage.getItem("record"));
   const myThemeSite = myRecord ? myRecord.themeSite : "interpark";
+  const myName = myRecord && myRecord.userName;
   //로딩
   const [loading, setLoading] = useState(true);
 
   //loadUserData
   useEffect(() => {
     loadUserData()
-      .then(
-        (res) => {
-          //정렬 후 저장
-          const sortedRecords = res.sort((a, b) => b.timeSpent - a.timeSpent);
-          setRecords(sortedRecords);
-          //기록 초기화면
-          const filter = (site) =>
-            sortedRecords.filter((item) => item.themeSite === site);
-          setFilteredRecords(filter(myThemeSite));
-          //내 기록 순위 (세션스토리지 / db 비교)
-          if (myRecord) {
-            const myRecordNanoSec = myRecord.timeStamp.nanoseconds;
-            const myRecordSec = myRecord.timeStamp.seconds;
-            setMyRecordIndex(
-              sortedRecords.findIndex(
-                (item) =>
-                  item.timeStamp.nanoseconds === myRecordNanoSec &&
-                  item.timeStamp.seconds === myRecordSec
-              )
-            );
-          }
-          setLoading(false);
-        },
-        [setLoading]
-      )
+      .then((res) => {
+        //정렬 후 저장
+        const sortedRecords = res.sort((a, b) => b.timeSpent - a.timeSpent);
+        setRecords(sortedRecords);
+        //기록 초기화면
+        const filter = (site) =>
+          sortedRecords.filter((item) => item.themeSite === site);
+        setFilteredRecords(filter(myThemeSite));
+        //내 기록 순위 (세션스토리지 / db 비교)
+        if (myRecord && filteredRecords) {
+          const myRecordNanoSec = myRecord.timeStamp.nanoseconds;
+          const myRecordSec = myRecord.timeStamp.seconds;
+          setMyRecordIndex(
+            filteredRecords.findIndex(
+              (item) =>
+                item.timeStamp.nanoseconds === myRecordNanoSec &&
+                item.timeStamp.seconds === myRecordSec &&
+                item.userName === myName
+            )
+          );
+        }
+        setLoading(false);
+      })
       .catch((error) =>
         console.error(`데이터를 가져오는 도중 에러 발생 : ${error}`)
       );
-  }, []);
-
+  }, [filteredRecords, myRecord, myName]);
   //시간 format 변경
   const formatTime = (time) => {
     return `${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, "0")}`;
@@ -72,7 +70,7 @@ const Record = () => {
   //현재 렌더링된 페이지
   const [activePage, setActivePage] = useState(1);
   useEffect(() => {
-    if (myRecordIndex !== null) {
+    if (myRecordIndex) {
       setActivePage(myRecordIndex ? Math.floor(myRecordIndex / 10) + 1 : 1);
     }
   }, [myRecordIndex]);
@@ -96,8 +94,10 @@ const Record = () => {
     const indexTrue =
       myRecordIndex === filteredRecords.findIndex((item) => item === record);
     const themeTrue = myThemeSite === clickThemeSite;
-    return indexTrue && themeTrue;
+    const nameTrue = myName === myRecord.userName;
+    return indexTrue && themeTrue && nameTrue;
   };
+
   const nav = useNavigate();
 
   if (loading) {
