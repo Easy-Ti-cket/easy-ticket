e22e27cdde18b5ded0ca554962f994520cdb3fca
hanheel <hanhee630630@gmail.com>
2024-09-03 12:21:45 +0900

    Fix: 기록이 없을 경우 화면 오류 해결

--- src/pages/challengeMode/record/Record.jsx
+++ src/pages/challengeMode/record/Record.jsx
@@ -1,104 +1,45 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import RecordNavigate from "../components/record/RecordNavigate";
 import loadUserData from "../../../apis/loadUserData";
 import Paging from "../../../util/Paging";
-
-const RecordContainer = styled.div`
-  width: 100vw;
-  background-color: var(--dimmed-color);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 5px;
-  padding: 50px 0;
-  text-align: center;
-`;
-const RecordTitle = styled.h2`
-  font-family: "pretendardB";
-  font-size: 36px;
-  color: var(--key-color);
-  padding: 20px;
-`;
-
-//네비게이터 + 기록
-const RecordContents = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-`;
-
-//행제목
-const RecordListTitle = styled.div`
-  display: flex;
-  margin-bottom: 20px;
-`;
-//전체 기록
-const RecordTable = styled.div`
-  padding: 20px 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: #fff;
-  border-radius: 8px;
-`;
-
-const RecordList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-`;
-// 순위 + 이름 + 기록
-const RecordItemContainer = styled.ul`
-  display: flex;
-  //로그인한 유저의 기록일 경우
-  box-shadow: ${(props) => props.$isUserRecord && "2px 2px 0 rgba(0,0,0,0.2)"};
-  border-radius: 8px;
-  background-color: ${(props) =>
-    props.$isUserRecord ? "var(--sub-color)" : "none"};
-  transition: background-color 0.2s ease;
-`;
-const RecordItem = styled.li`
-  list-style-type: none;
-  width: 100px;
-  justify-content: center;
-  padding: 15px;
-  font-family: "pretendardB";
-  color: ${(props) =>
-    props.$bold ? "var(--text-color2)" : "var(--text-color)"};
-  color: ${(props) => props.$highlight && "var(--key-color)"};
-  color: ${(props) => props.$isUserRecord && "var(--key-color)"};
-  font-size: ${(props) => props.$isUserRecord && "24px"};
-`;
+//css
+import {
+  RecordContainer,
+  RecordContents,
+  RecordItem,
+  RecordItemContainer,
+  RecordList,
+  RecordListTitle,
+  RecordTable,
+  RecordTitle
+} from "./RecordStyle";
 
 const Record = () => {
-  //db로부터 받은 데이터 + 정렬
+  //db로부터 받은 데이터 (정렬됨)
   const [records, setRecords] = useState([]);
-  //필터링된 데이터 (초기값 인터파크)
+  //필터링된 데이터
   const [filteredRecords, setFilteredRecords] = useState([]);
   //내 데이터 순위
-  const [myRecordIndex, setMyRecordIndex] = useState(-1);
+  const [myRecordIndex, setMyRecordIndex] = useState(null);
   //세션스토리지에 저장된 record
   const myRecord = JSON.parse(sessionStorage.getItem("record"));
-  const myThemeSite = JSON.parse(sessionStorage.getItem("record")).themeSite;
+  const myThemeSite = myRecord ? myRecord.themeSite : "interpark";
 
-  // 데이터베이스로부터 데이터 읽어오기 - loadUserData 함수
+  //loadUserData
   useEffect(() => {
     loadUserData()
       .then((res) => {
         //정렬 후 저장
         const sortedRecords = res.sort((a, b) => b.timeSpent - a.timeSpent);
         setRecords(sortedRecords);
-        //기록 초기화면 - 사용자가 예매를 진행한 사이트
-        setFilteredRecords(
-          sortedRecords.filter((item) => item.themeSite === myThemeSite)
-        );
+        //기록 초기화면
+        const filter = (site) =>
+          sortedRecords.filter((item) => item.themeSite === site);
+        setFilteredRecords(filter(myThemeSite));
         //내 기록 순위 (세션스토리지 / db 비교)
-        const myRecordNanoSec = myRecord.timeStamp.nanoseconds;
-        const myRecordSec = myRecord.timeStamp.seconds;
         if (myRecord) {
+          const myRecordNanoSec = myRecord.timeStamp.nanoseconds;
+          const myRecordSec = myRecord.timeStamp.seconds;
           setMyRecordIndex(
             sortedRecords.findIndex(
               (item) =>
@@ -119,10 +60,10 @@ const Record = () => {
   };
 
   //페이지네이션
-  //현재 페이지
+  //현재 렌더링된 페이지
   const [activePage, setActivePage] = useState(1);
   useEffect(() => {
-    setActivePage(myRecordIndex ? Math.floor(myRecordIndex / 10 + 1) : 1);
+    setActivePage(myRecordIndex ? Math.floor(myRecordIndex / 10) + 1 : 1);
   }, [myRecordIndex]);
   //한 페이지 당 보여줄 아이템 개수
   const itemsCountPerPage = 10;
@@ -138,7 +79,7 @@ const Record = () => {
     activePage * itemsCountPerPage
   );
 
-  //올바른 themeSite와 순위에서 css 변경
+  //사용자가 진행한 themeSite와 기록 순위에서 css 변경
   const [clickThemeSite, setClickThemeSite] = useState(myThemeSite);
   const isUserRecord = (record) => {
     const indexTrue =
@@ -146,7 +87,6 @@ const Record = () => {
     const themeTrue = myThemeSite === clickThemeSite;
     return indexTrue && themeTrue;
   };
-  console.log(clickThemeSite);
 
   return (
     <RecordContainer>
