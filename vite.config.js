776d4c448c07f3393b100486ec60d2714c1cd162
abyss-s <xyz@inha.edu>
2024-09-08 13:49:29 +0900

    Chore: 정적파일 업로드를 위한 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,21 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500 // 청크 사이즈 경고를 500kb로 설정
+  }
 });
