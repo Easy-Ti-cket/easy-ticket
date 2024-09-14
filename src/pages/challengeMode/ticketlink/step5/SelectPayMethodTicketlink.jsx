848b524a79d65550d9cb514455705057367c90ed
다은 <drechsu@gmail.com>
2024-09-15 01:21:17 +0900

    Fix: 실전모드 progressStep 설정

--- src/pages/challengeMode/ticketlink/step5/SelectPayMethodTicketlink.jsx
+++ src/pages/challengeMode/ticketlink/step5/SelectPayMethodTicketlink.jsx
@@ -1,12 +1,17 @@
 import styled from "styled-components";
 import SelectPayMethodChallenge from "../../components/payMethod/SelectPayMethodChallenge";
-
+import { useSetAtom } from "jotai";
+import { progressAtom } from "../../../../store/atom";
+import { useEffect } from "react";
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
 const SelectPayMethodTicketlink = () => {
+  const setProgress = useSetAtom(progressAtom);
+  useEffect(() => setProgress(5), []);
+
   return (
     <Wrap>
       <SelectPayMethodChallenge />
