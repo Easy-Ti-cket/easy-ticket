dc5189dc1aff58e7365293b292a12c4934acd37a
영 <77565980+abyss-s@users.noreply.github.com>
2024-09-14 21:55:19 +0900

    Fix: posters 폴더 내 이미지만 압축되도록 수정

--- compress-images.js
+++ compress-images.js
@@ -2,44 +2,55 @@ import imagemin from "imagemin";
 import imageminPngquant from "imagemin-pngquant";
 import imageminSvgo from "imagemin-svgo";
 import path from "path";
-import fs from "fs";
 import glob from "glob";
 import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// assets/images 폴더 경로 설정
-const imagesFolder = path.join(__dirname, "public", "assets", "images");
+// 포스터 폴더 설정
+const postersFolder = path.join(
+  __dirname,
+  "public",
+  "assets",
+  "images",
+  "posters"
+);
 
 const compressImages = async () => {
-  const files = glob.sync(`${assetsFolder}/**/*.{png,svg}`);
+  try {
+    // posters 폴더 내의 SVG 파일 찾기
+    const files = glob.sync(`${postersFolder}/**/*.svg`);
 
-  if (files.length === 0) {
-    return;
-  }
+    if (files.length === 0) {
+      return;
+    }
 
-  await Promise.all(
-    files.map(async (file) => {
-      const outputPath = file; // 압축된 파일을 원본 파일 위치에 저장
+    await Promise.all(
+      files.map(async (file) => {
+        const outputPath = file; // 압축된 파일을 원본 파일 위치에 저장
 
-      const [fileData] = await imagemin([file], {
-        destination: path.dirname(outputPath),
-        plugins: [
-          imageminPngquant({ quality: [0.6, 0.8] }), // PNG 파일 압축
-          imageminSvgo({
-            // SVG 파일 최적화
+        try {
+          const [fileData] = await imagemin([file], {
+            destination: path.dirname(outputPath),
             plugins: [
-              { name: "removeViewBox", active: false },
-              { name: "removeDimensions", active: true }
+              imageminSvgo({
+                // SVG 파일 최적화
+                plugins: [
+                  { name: "removeViewBox", active: false },
+                  { name: "removeDimensions", active: true }
+                ]
+              })
             ]
-          })
-        ]
-      });
-      // console.log(`Image optimized: ${fileData.sourcePath}`);
-    })
-  );
+          });
+        } catch (err) {
+          // console.error(`Error optimizing file ${file}:`, err);
+        }
+      })
+    );
+  } catch (err) {
+    console.error("Error during compression:", err);
+  }
 };
 
-compressImages().catch((err) =>
-  console.error("Error during compression:", err)
-);
+compressImages();
