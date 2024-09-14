e1cd7eba37c2bd1c378b8c133afd82b6fd028b58
영 <77565980+abyss-s@users.noreply.github.com>
2024-09-14 21:50:25 +0900

    Chore: 이미지 원본 파일 경로에 저장하도록 수정

--- compress-images.js
+++ compress-images.js
@@ -2,12 +2,14 @@ import imagemin from "imagemin";
 import imageminPngquant from "imagemin-pngquant";
 import imageminSvgo from "imagemin-svgo";
 import path from "path";
+import fs from "fs";
 import glob from "glob";
 import { fileURLToPath } from "url";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const assetsFolder = path.join(__dirname, "public", "assets");
+
+// assets/images 폴더 경로 설정
+const imagesFolder = path.join(__dirname, "public", "assets", "images");
 
 const compressImages = async () => {
   const files = glob.sync(`${assetsFolder}/**/*.{png,svg}`);
@@ -16,23 +18,26 @@ const compressImages = async () => {
     return;
   }
 
-  //console.log("Files found:", files);
+  await Promise.all(
+    files.map(async (file) => {
+      const outputPath = file; // 압축된 파일을 원본 파일 위치에 저장
 
-  const compressedFiles = await imagemin(files, {
-    destination: assetsFolder, // 압축된 파일을 저장할 경로
-    plugins: [
-      imageminPngquant({ quality: [0.6, 0.8] }), // PNG 파일 압축
-      imageminSvgo({
-        // SVG 파일 최적화
+      const [fileData] = await imagemin([file], {
+        destination: path.dirname(outputPath),
         plugins: [
-          { name: "removeViewBox", active: false },
-          { name: "removeDimensions", active: true }
+          imageminPngquant({ quality: [0.6, 0.8] }), // PNG 파일 압축
+          imageminSvgo({
+            // SVG 파일 최적화
+            plugins: [
+              { name: "removeViewBox", active: false },
+              { name: "removeDimensions", active: true }
+            ]
+          })
         ]
-      })
-    ]
-  });
-
-  //console.log("Images optimized:", compressedFiles);
+      });
+      // console.log(`Image optimized: ${fileData.sourcePath}`);
+    })
+  );
 };
 
 compressImages().catch((err) =>
